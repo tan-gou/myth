93d3cfe9569f5cab02b7d80122b1e294058256df
haibing.cao <haibing.cao>
2019-01-21 16:49:59 +0800

    code optimize

--- myth-core/src/main/java/com/github/myth/core/disruptor/handler/MythTransactionEventHandler.java
+++ myth-core/src/main/java/com/github/myth/core/disruptor/handler/MythTransactionEventHandler.java
@@ -28,12 +28,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
- * <p>Description: .</p>
- *
- * @author xiaoyu(Myth)
- * @version 1.0
- * @date 2018/3/5 11:52
- * @since JDK 1.8
+ * MythTransaction 事务事件处理器
  */
 @Component
 public class MythTransactionEventHandler implements EventHandler<MythTransactionEvent> {
@@ -42,13 +37,15 @@ public class MythTransactionEventHandler implements EventHandler<MythTransaction
     private CoordinatorService coordinatorService;
 
     @Override
-    public void onEvent(MythTransactionEvent mythTransactionEvent,
-                        long sequence, boolean endOfBatch) {
+    public void onEvent(MythTransactionEvent mythTransactionEvent, long sequence, boolean endOfBatch) {
+
         if (mythTransactionEvent.getType() == EventTypeEnum.SAVE.getCode()) {
             coordinatorService.save(mythTransactionEvent.getMythTransaction());
-        } else if (mythTransactionEvent.getType() == EventTypeEnum.UPDATE_PARTICIPANT.getCode()) {
+        }
+        else if (mythTransactionEvent.getType() == EventTypeEnum.UPDATE_PARTICIPANT.getCode()) {
             coordinatorService.updateParticipant(mythTransactionEvent.getMythTransaction());
-        } else if (mythTransactionEvent.getType() == EventTypeEnum.UPDATE_STATUS.getCode()) {
+        }
+        else if (mythTransactionEvent.getType() == EventTypeEnum.UPDATE_STATUS.getCode()) {
             final MythTransaction mythTransaction = mythTransactionEvent.getMythTransaction();
             coordinatorService.updateStatus(mythTransaction.getTransId(), mythTransaction.getStatus());
         }
