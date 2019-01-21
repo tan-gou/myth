8bd4eea74cf50702a4a80ed2abfcd03c877810fc
haibing.cao <haibing.cao>
2019-01-21 15:44:29 +0800

    code optimize

--- myth-annotation/src/main/java/com/github/myth/annotation/PropagationEnum.java
+++ myth-annotation/src/main/java/com/github/myth/annotation/PropagationEnum.java
@@ -1,62 +1,18 @@
-/*
- *
- * Copyright 2017-2018 549477611@qq.com(xiaoyu)
- *
- * This copyrighted material is made available to anyone wishing to use, modify,
- * copy, or redistribute it subject to the terms and conditions of the GNU
- * Lesser General Public License, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
- * for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this distribution; if not, see <http://www.gnu.org/licenses/>.
- *
- */
 package com.github.myth.annotation;
 
-
 /**
- * @author xiaoyu
+ * 事务的传播级别
  */
-
 public enum PropagationEnum {
 
-    /**
-     * PropagationEnum required propagation.
-     */
     PROPAGATION_REQUIRED(0),
 
-    /**
-     * PropagationEnum supports propagation.
-     */
     PROPAGATION_SUPPORTS(1),
-
-    /**
-     * PropagationEnum mandatory propagation.
-     */
     PROPAGATION_MANDATORY(2),
-
-    /**
-     * PropagationEnum requires new propagation.
-     */
     PROPAGATION_REQUIRES_NEW(3),
 
-    /**
-     * PropagationEnum not supported propagation.
-     */
     PROPAGATION_NOT_SUPPORTED(4),
-
-    /**
-     * PropagationEnum never propagation.
-     */
     PROPAGATION_NEVER(5),
-
-    /**
-     * PropagationEnum nested propagation.
-     */
     PROPAGATION_NESTED(6);
 
 
@@ -66,14 +22,7 @@ public enum PropagationEnum {
         this.value = value;
     }
 
-    /**
-     * Gets value.
-     *
-     * @return the value
-     */
     public int getValue() {
         return this.value;
     }
-
-
 }
