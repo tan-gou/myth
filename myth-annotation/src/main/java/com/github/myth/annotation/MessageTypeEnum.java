99bb888f9415725bc34b92e98d3034957aecc11f
haibing.cao <haibing.cao>
2019-01-21 15:45:51 +0800

    code optimize

--- myth-annotation/src/main/java/com/github/myth/annotation/MessageTypeEnum.java
+++ myth-annotation/src/main/java/com/github/myth/annotation/MessageTypeEnum.java
@@ -1,45 +1,16 @@
-/*
- *
- * Copyright 2017-2018 549477611@qq.com(xiaoyu)
- *
- * This copyrighted material is made available to anyone wishing to use, modify,
- * copy, or redistribute it subject to the terms and conditions of the GNU
- * Lesser General Public License, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
- * for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this distribution; if not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 package com.github.myth.annotation;
 
 /**
  * 消息类型枚举
- *
- * @author xiaoyu
  */
-@SuppressWarnings("ALL")
 public enum MessageTypeEnum {
 
-
-    /**
-     * P 2 p message type enum.
-     */
     P2P(1, "点对点模式"),
 
-
-    /**
-     * Topic message type enum.
-     */
-    TOPIC(2, "TOPIC模式");
+    TOPIC(2, "TOPIC模式"),
+    ;
 
     private Integer code;
-
     private String desc;
 
     MessageTypeEnum(Integer code, String desc) {
@@ -47,38 +18,17 @@ public enum MessageTypeEnum {
         this.desc = desc;
     }
 
-    /**
-     * Gets code.
-     *
-     * @return the code
-     */
+
     public Integer getCode() {
         return code;
     }
-
-    /**
-     * Sets code.
-     *
-     * @param code the code
-     */
     public void setCode(Integer code) {
         this.code = code;
     }
 
-    /**
-     * Gets desc.
-     *
-     * @return the desc
-     */
     public String getDesc() {
         return desc;
     }
-
-    /**
-     * Sets desc.
-     *
-     * @param desc the desc
-     */
     public void setDesc(String desc) {
         this.desc = desc;
     }
