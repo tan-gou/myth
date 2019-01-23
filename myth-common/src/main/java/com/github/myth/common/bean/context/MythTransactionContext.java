f1e8bf18f3ea28c853940c86c680ebe071fc8896
haibing.cao <haibing.cao>
2019-01-23 09:48:51 +0800

    code optimize

--- myth-common/src/main/java/com/github/myth/common/bean/context/MythTransactionContext.java
+++ myth-common/src/main/java/com/github/myth/common/bean/context/MythTransactionContext.java
@@ -1,20 +1,3 @@
-/*
- *
- * Copyright 2017-2018 549477611@qq.com(xiaoyu)
- *
- * This copyrighted material is made available to anyone wishing to use, modify,
- * copy, or redistribute it subject to the terms and conditions of the GNU
- * Lesser General Public License, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
- * for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this distribution; if not, see <http://www.gnu.org/licenses/>.
- *
- */
 package com.github.myth.common.bean.context;
 
 import lombok.Data;
@@ -23,7 +6,7 @@ import java.io.Serializable;
 
 
 /**
- * @author xiaoyu
+ * 事务上下文
  */
 @Data
 public class MythTransactionContext implements Serializable {
@@ -32,11 +15,8 @@ public class MythTransactionContext implements Serializable {
 
     private String transId;
 
-
     /**
      * 事务参与的角色 {@linkplain com.github.myth.common.enums.MythRoleEnum}
      */
     private int role;
-
-
 }
