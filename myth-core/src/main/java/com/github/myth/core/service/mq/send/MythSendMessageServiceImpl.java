680b69df1c4a47bb9cb24401362e9ab6f4c2efaf
haibing.cao <haibing.cao>
2019-01-23 17:35:35 +0800

    code optimize

--- myth-core/src/main/java/com/github/myth/core/service/mq/send/MythSendMessageServiceImpl.java
+++ myth-core/src/main/java/com/github/myth/core/service/mq/send/MythSendMessageServiceImpl.java
@@ -17,17 +17,9 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Objects;
 
-/**
- * <p>Description: .</p>
- *
- * @author xiaoyu(Myth)
- * @version 1.0
- * @date 2018/4/14 17:47
- * @since JDK 1.8
- */
+
 @Service("mythSendMessageService")
 public class MythSendMessageServiceImpl implements MythSendMessageService {
-
 
     private volatile ObjectSerializer serializer;
 
@@ -39,8 +31,8 @@ public class MythSendMessageServiceImpl implements MythSendMessageService {
     /**
      * 发送消息
      *
-     * @param mythTransaction 消息体
-     * @return true 处理成功  false 处理失败
+     * @param   mythTransaction 消息体
+     * @return  true 处理成功  false 处理失败
      */
     @Override
     public Boolean sendMessage(MythTransaction mythTransaction) {
@@ -48,7 +40,7 @@ public class MythSendMessageServiceImpl implements MythSendMessageService {
             return false;
         }
         final List<MythParticipant> mythParticipants = mythTransaction.getMythParticipants();
-        /*
+        /**
          * 这里的这个判断很重要，不为空，表示本地的方法执行成功，需要执行远端的rpc方法
          * 为什么呢，因为我会在切面的finally里面发送消息，意思是切面无论如何都需要发送mq消息
          * 那么考虑问题，如果本地执行成功，调用rpc的时候才需要发
@@ -58,20 +50,19 @@ public class MythSendMessageServiceImpl implements MythSendMessageService {
 
             for (MythParticipant mythParticipant : mythParticipants) {
                 MessageEntity messageEntity =
-                        new MessageEntity(mythParticipant.getTransId(),
-                                mythParticipant.getMythInvocation());
+                        new MessageEntity(mythParticipant.getTransId(), mythParticipant.getMythInvocation());
                 try {
                     final byte[] message = getObjectSerializer().serialize(messageEntity);
-                    getMythMqSendService().sendMessage(mythParticipant.getDestination(),
-                            mythParticipant.getPattern(),
-                            message);
+                    // 发送 MQ 消息
+                    getMythMqSendService().sendMessage(mythParticipant.getDestination(), mythParticipant.getPattern(), message);
                 } catch (Exception e) {
                     e.printStackTrace();
                     return Boolean.FALSE;
                 }
             }
-            //这里为什么要这么做呢？ 主要是为了防止在极端情况下，发起者执行过程中，突然自身down 机
-            //造成消息未发送，新增一个状态标记，如果出现这种情况，通过定时任务发送消息
+
+            // 这里为什么要这么做呢？ 主要是为了防止在极端情况下，发起者执行过程中，突然自身down 机
+            // 造成消息未发送，新增一个状态标记，如果出现这种情况，通过定时任务发送消息
             mythTransaction.setStatus(MythStatusEnum.COMMIT.getCode());
             publisher.publishEvent(mythTransaction, EventTypeEnum.UPDATE_STATUS.getCode());
         }
