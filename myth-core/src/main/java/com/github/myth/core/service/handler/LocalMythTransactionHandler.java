8a1e252c3e8da4f229765431061dd5abe28653eb
haibing.cao <haibing.cao>
2019-01-23 10:04:34 +0800

    code optimize

--- myth-core/src/main/java/com/github/myth/core/service/handler/LocalMythTransactionHandler.java
+++ myth-core/src/main/java/com/github/myth/core/service/handler/LocalMythTransactionHandler.java
@@ -1,21 +1,3 @@
-/*
- *
- * Copyright 2017-2018 549477611@qq.com(xiaoyu)
- *
- * This copyrighted material is made available to anyone wishing to use, modify,
- * copy, or redistribute it subject to the terms and conditions of the GNU
- * Lesser General Public License, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
- * for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this distribution; if not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 package com.github.myth.core.service.handler;
 
 import com.github.myth.common.bean.context.MythTransactionContext;
@@ -23,25 +5,21 @@ import com.github.myth.core.service.MythTransactionHandler;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.stereotype.Component;
 
-/**
- * @author xiaoyu
- */
+
 @Component
 public class LocalMythTransactionHandler implements MythTransactionHandler {
-
 
     /**
      * Myth分布式事务处理接口
      *
-     * @param point                  point 切点
-     * @param mythTransactionContext myth事务上下文
-     * @return Object
-     * @throws Throwable 异常
+     * @param   point                  point 切点
+     * @param   mythTransactionContext myth事务上下文
+     * @return  Object
+     * @throws  Throwable 异常
      */
     @Override
     public Object handler(ProceedingJoinPoint point, MythTransactionContext mythTransactionContext) throws Throwable {
 
         return point.proceed();
-
     }
 }
